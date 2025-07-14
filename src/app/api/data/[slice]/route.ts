0ab70f34efc585369c3dca03aa7ec0177bd966ec
Gautam Kumar <104510672+Developer-Gautam@users.noreply.github.com>
2025-07-14 20:18:55 +0530

    Update route.ts

--- src/app/api/data/[slice]/route.ts
+++ src/app/api/data/[slice]/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 import { promises as fs } from 'fs';
 
-export async function GET(req: NextRequest, context: { params: { slice: string } }) {
-  const { slice } = context.params;
+export async function GET(req: NextRequest, { params }: any) {
+  const { slice } = params;
   const dataDir = path.join(process.cwd(), 'src', 'json_data');
   const filePath = path.join(dataDir, `${slice}.json`);
   try {
